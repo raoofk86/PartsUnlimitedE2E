0139b510f0e73da25b98cd214af1b026783dcf97
Abdul Raoof <abdulraoof.k86@hotmail.com>
2020-11-15 11:00:24 +0000

    added comments

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
@@ -3,12 +3,15 @@ using System.ComponentModel.DataAnnotations;
 
 namespace PartsUnlimited.Models
 {
+    // commented
     public class CartItem : ILineItem
     {
+        
         [Key]
         public int CartItemId { get; set; }
 
         [Required]
+        
         public string CartId { get; set; }
         public int ProductId { get; set; }
         public int Count { get; set; }
